3160fd0957057a714fad4f2b8f4f9ff73ad2d9f3
Gaurav Pareek <me@grv.rocks>
2016-02-17 17:08:06 +0530

    Moved around stuf in footer

--- footer.php
+++ footer.php
@@ -4,30 +4,15 @@
 
 			<div class="wrap">
 
-				<div class="ubuntugnome_logo footer-section footer-1">
-					<?php if ( get_theme_mod( 'ubuntugnome_logo' ) ) : ?>
-						<img src="<?php echo get_theme_mod( 'ubuntugnome_logo', $default=true ); ?>" alt="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" />
-					<?php else: ?>
-						<p>Please upload a logo under Appearance >> Customize >> Ubuntu GNOME</p>
-					<?php endif; ?>
-				</div>
-
-				<div class="ubuntugnome_licence footer-section footer-2">
+				<div class="ubuntugnome_licence footer-section footer-1">
 					<?php
 					$licensetext = 'Content licensed under.';
 					echo get_theme_mod( 'ubuntugnome_license_text', $licensetext );
-					?><br />
-					<img class="icons" src="<?php echo get_theme_mod( 'ubuntugnome_license_image', true ); ?>" alt="Copyleft, Creative commons, Public domain" />
-				</div>
-
-				<div class="ubuntugnome_trademark footer-section footer-3">
-					<?php
-					$tmtext = 'Ubuntu is a trademark of Canonical.<br>GNOME is a trademark of the GNOME Foundation.<br>Used by permission.';
-					echo get_theme_mod( 'ubuntugnome_trademark_text', $tmtext );
 					?>
+					<?php hybrid_get_menu( 'secondary' ); // Loads the menu/secondary.php template. ?>
 				</div>
 
-				<div class="ubuntugnome_social footer-section footer-4">
+				<div class="ubuntugnome_social footer-section footer-2">
 					<?php hybrid_get_menu( 'subsidiary' ); // Loads the menu/secondary.php template. ?>
 				</div>
 
