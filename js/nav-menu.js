3e679668f28de63ff94d2d4850661246ae92d8fc
Gaurav Pareek <me@grv.rocks>
2016-02-29 11:54:26 +0530

    Fixed menu icon not loading

--- js/nav-menu.js
+++ js/nav-menu.js
@@ -4,10 +4,10 @@
 			var width = jQuery( window ).width();
 
 			if ( 980 <= width ) {
-        jQuery( '.menu > .wrap' ).fadeIn();
+        jQuery( '#menu-primary > .wrap' ).fadeIn();
 				//jQuery( '.menu-toggle' ).removeClass( 'active' )
 			} else {
-        jQuery( '.menu > .wrap' ).hide();
+        jQuery( '#menu-primary > .wrap' ).hide();
 				//jQuery( '.menu > .wrap:visible' ).parent().children( '.menu-toggle' ).addClass( 'active' );
 			}
 		}
